28f93ee3abbf8ccd5fd9702e4ff3593ad3f3daf4
gerardo <up240497@alumnos.upa.edu.mxo>
2025-12-02 15:18:03 -0600

    agregar clases base para el conversor

---
+++ ConversorLongitud.java
@@ -0,0 +1,11 @@
+package com.equipo3.conversor;
+
+public class ConversorLongitud {
+
+    public double convertir(double valor,
+                            UnidadLongitud origen,
+                            UnidadLongitud destino) {
+        // TODO: implementar en la rama feature/longitud
+        return 0.0;
+    }
+}
---
+++ ConversorMasa.java
@@ -0,0 +1,11 @@
+package com.equipo3.conversor;
+
+public class ConversorMasa {
+
+    public double convertir(double valor,
+                            UnidadMasa origen,
+                            UnidadMasa destino) {
+        // TODO: implementar en la rama feature/masa
+        return 0.0;
+    }
+}
